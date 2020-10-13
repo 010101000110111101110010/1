3eb698c07aee02b1bec1859703ca967af7b23e3f
010101000110111101110010 <maks.ogloblin.2002@mail.ru>
2020-10-13 17:26:19 +0900

    Initial commit

---
+++ 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib1;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lib asd = new lib();
+            lib.sloj(3,4);
+            lib.vichet(4, 56);
+            lib.age(35);
+            Console.WriteLine("Введите новый логин:");
+            Console.WriteLine($"Ваш новый логин - {lib.newlogin()}");
+            Console.WriteLine("Нахождение дискриминанта: введите a, b, c");
+            Console.WriteLine($"D={lib.D()}");
+            Console.ReadLine();
+        }
+    }
+}
